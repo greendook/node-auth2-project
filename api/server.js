973f68e8e1a8ae683c85273d1242943a9f9f93f8
xavier <xavierhoskins@outlook.com>
2020-05-19 23:27:55 -0500

    mvp

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const usersRouter = require('../users/users-router.js');
+const authRouter = require('../auth/auth-router.js');
+const authenticator = require('../auth/authenticator.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/users', authenticator, usersRouter);
+server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+  res.json({ message: 'works' });
+});
+
+module.exports = server;
